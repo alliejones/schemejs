b6b11462791ebe7b0dd59d858938ffc377b7654e
Allie Jones <allie@alliejon.es>
2016-01-14 12:14:36 -0500

    Configure babel

--- interpreter.js
+++ interpreter.js
@@ -1,11 +1,18 @@
-function evalScheem (expr, env) {
+var lookup = function (env, v) {
+  if (env.bindings.hasOwnProperty(v))
+    return env.bindings[v];
+  else
+    return lookup(env.outer, v);
+};
+
+function evl (expr, env) {
   // Numbers evaluate to themselves
   if (typeof expr === 'number') {
     return expr;
   }
 
   if (typeof expr === 'string') {
-    return env[expr];
+    return lookup(env, expr);
   }
 
   // Look at head of list for operation
@@ -13,71 +20,76 @@ function evalScheem (expr, env) {
   var val, val2, list;
   switch (op) {
     case '+':
-      return evalScheem(expr[1], env) + evalScheem(expr[2], env);
+      return evl(expr[1], env) + evl(expr[2], env);
 
     case '-':
-      return evalScheem(expr[1], env) - evalScheem(expr[2], env);
+      return evl(expr[1], env) - evl(expr[2], env);
 
     case '*':
-      return evalScheem(expr[1], env) * evalScheem(expr[2], env);
+      return evl(expr[1], env) * evl(expr[2], env);
 
     case '/':
-      return evalScheem(expr[1], env) / evalScheem(expr[2], env);
+      return evl(expr[1], env) / evl(expr[2], env);
 
     case '=':
-      val = (evalScheem(expr[1], env) === evalScheem(expr[2], env));
+      val = (evl(expr[1], env) === evl(expr[2], env));
       return val ? '#t' : '#f';
 
     case '>':
-      val = (evalScheem(expr[1], env) > evalScheem(expr[2], env));
+      val = (evl(expr[1], env) > evl(expr[2], env));
       return val ? '#t' : '#f';
 
     case '<':
-      val = (evalScheem(expr[1], env) < evalScheem(expr[2], env));
+      val = (evl(expr[1], env) < evl(expr[2], env));
       return val ? '#t' : '#f';
 
     case 'quote':
       return expr[1];
 
     case 'define':
-      env[expr[1]] = evalScheem(expr[2], env);
+      env[expr[1]] = evl(expr[2], env);
       return 0;
+
+    case 'let-one':
+      var newEnv = { bindings: {}, outer: env };
+      bindings[expr[1]] = evl(expr[2], env);
+      return evl(expr[3], newEnv);
 
     case 'set!':
       if (!env.hasOwnProperty(expr[1]))
         throw new Error('Variable '+expr[1]+' is not defined');
 
-      env[expr[1]] = evalScheem(expr[2], env);
+      env[expr[1]] = evl(expr[2], env);
       return 0;
 
     case 'begin':
       for (var i = 1; i < expr.length; i++) {
-        val = evalScheem(expr[i], env);
+        val = evl(expr[i], env);
       }
       return val;
 
     case 'cons':
-      val = evalScheem(expr[1], {});
-      list = evalScheem(expr[2], {});
+      val = evl(expr[1], {});
+      list = evl(expr[2], {});
       return [val].concat(list);
 
     case 'car':
-      list = evalScheem(expr[1], {});
+      list = evl(expr[1], {});
       val = list.shift();
       return val;
 
     case 'cdr':
-      list = evalScheem(expr[1], {});
+      list = evl(expr[1], {});
       list.shift();
       return list;
 
     case 'if':
-      val = evalScheem(expr[1], env);
+      val = evl(expr[1], env);
       if (expr[1] === '#t')
-        return evalScheem(expr[2], env);
+        return evl(expr[2], env);
       else if (val === '#f')
-        return evalScheem(expr[3], env);
+        return evl(expr[3], env);
   }
 };
 
-module.exports = evalScheem;
+module.exports = evl;
