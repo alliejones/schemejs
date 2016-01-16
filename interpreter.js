e89d28d126675f62c2588bc5dbd7e8dc7b114697
Allie Jones <allie@alliejon.es>
2016-01-16 16:55:41 -0500

    Refactor each special form to its own function

--- interpreter.js
+++ interpreter.js
@@ -1,5 +1,82 @@
 var initialEnv = require('./initialEnv.js')
 
+var evl = function evl (expr, env = initialEnv) {
+  // Numbers evaluate to themselves
+  if (typeof expr === 'number') {
+    return expr;
+
+  } else if (typeof expr === 'string') {
+    return lookup(env, expr);
+
+  } else if (specialForms.hasOwnProperty(expr[0])) {
+    let args = [env].concat(expr.slice(1));
+    return specialForms[expr[0]].apply(null, args);
+
+  } else {
+    // user-defined function
+    let func = evl(expr[0], env);
+    let args = expr.slice(1);
+    args = args.map(a => evl(a, env));
+    return func.apply(null, args);
+  }
+};
+
+
+var specialForms = {
+  quote: function (env, expr) {
+    return expr;
+  },
+
+  define: function (env, name, val) {
+    env[name] = evl(val, env);
+  },
+
+  // ES5 indeed allows reserved words as property names
+  let: function (env, decl, body) {
+    var newEnv = { bindings: {}, outer: env };
+    for (var i = 0; i < decl.length; i++) {
+      let [ k, v ] = decl[i];
+      newEnv.bindings[k] = evl(v, env);
+    }
+    return evl(body, newEnv);
+  },
+
+  'set!': function (env, name, val) {
+    update(env, name, evl(val, env));
+    return 0;
+  },
+
+  begin: function (env, ...exprs) {
+    var val;
+    for (var i = 0; i < exprs.length; i++) {
+      val = evl(exprs[i], env);
+    }
+    return val;
+  },
+
+  if: function (env, cond, whenTrue, whenFalse) {
+    // everthing in Scheme is truthy but #f
+    if (evl(cond, env) === '#f')
+      return evl(whenFalse, env);
+    else
+      return evl(whenTrue, env);
+  },
+
+  lambda: function (env, ...args) {
+    var argNames = args;
+    var body = argNames.pop();
+    return function () {
+      var argVals = [].slice.apply(arguments);
+      var newEnv = { bindings: {}, outer: env };
+      for (var i = 0; i < argNames.length; i++) {
+        newEnv.bindings[argNames[i]] = argVals[i];
+      }
+      return evl(body, newEnv);
+    };
+  }
+};
+
+
 var lookup = function (env, v) {
   if (env.bindings && env.bindings.hasOwnProperty(v))
     return env.bindings[v];
@@ -10,86 +87,19 @@ var lookup = function (env, v) {
     }
 };
 
+
 var update = function (env, v, val) {
   if (env.bindings && env.bindings.hasOwnProperty(v))
     env.bindings[v] = val;
   else if (env.outer)
     update(env.outer, v, val);
   else
-    throw new Error(`Variable ${v} is not defined.`);
+    throw new Error(`Value ${v} is not defined.`);
 };
+
 
 var addBinding = function (env, v, val) {
   env.bindings[v] = val;
-};
-
-function evl (expr, env = initialEnv) {
-  // Numbers evaluate to themselves
-  if (typeof expr === 'number') {
-    return expr;
-  }
-
-  if (typeof expr === 'string') {
-    return lookup(env, expr);
-  }
-
-  // Look at head of list for operation
-  var op = expr[0];
-  var val, val2, list;
-  switch (op) {
-
-    case 'quote':
-      return expr[1];
-
-    case 'define':
-      env[expr[1]] = evl(expr[2], env);
-      return 0;
-
-    case 'let':
-      var newEnv = { bindings: {}, outer: env };
-      var decl = expr[1];
-      for (var i = 0; i < decl.length; i++) {
-        let [ k, v ] = decl[i];
-        newEnv.bindings[k] = evl(v, env);
-      }
-      return evl(expr[2], newEnv);
-
-    case 'set!':
-      update(env, expr[1], evl(expr[2], env));
-      return 0;
-
-    case 'begin':
-      for (var i = 1; i < expr.length; i++) {
-        val = evl(expr[i], env);
-      }
-      return val;
-
-    case 'if':
-      val = evl(expr[1], env);
-      // everthing in Scheme is truthy but #f
-      if (val === '#f')
-        return evl(expr[3], env);
-      else
-        return evl(expr[2], env);
-
-    case 'lambda':
-      var args = expr.slice(1, -1);
-      return function () {
-        var argVals = [].slice.apply(arguments);
-        var newEnv = { bindings: {}, outer: env };
-        for (var i = 0; i < args.length; i++) {
-          newEnv.bindings[args[i]] = argVals[i];
-        }
-
-        return evl(expr[expr.length - 1], newEnv);
-      };
-
-    default:
-      var func = evl(expr[0], env);
-      var args = expr.slice(1);
-      args = args.map(a => evl(a, env));
-      return func.apply(null, args);
-  }
 };
 
 module.exports = evl;
